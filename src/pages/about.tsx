25df76fc23ab24df785f67c5a44e367083fbf5a0
VanshSharmaPES <vanshsharma2006asr@gmail.com>
2025-11-29 18:58:24 +0530

    Added information to the About Page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,7 +14,8 @@ export default function About() {
           
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-12 items-start">
             <div className="lg:col-span-2 space-y-6">
-              <p className="text-lg text-surface-light leading-relaxed whitespace-pre-line">
+              {/* CHANGE HERE: Added 'text-justify' and removed 'whitespace-pre-line' */}
+              <p className="text-lg text-surface-light leading-relaxed text-justify"> 
                 {PORTFOLIO_DATA.about}
               </p>
               <p className="text-lg font-semibold text-neutral-text">
